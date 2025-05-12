aa3ca4c87c1462fc5f33e065b3b8a4a2cd4b9d72
Brendan Irvine-Broque <birvine-broque@cloudflare.com>
2025-05-12 16:40:21 -0700

    works when sending to right dd region

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,9 @@ export class MyDurableObject extends DurableObject {
 					appKeyAuth: env.DATADOG_APP_KEY,
 				},
 			});
+			configuration.setServerVariables({
+				site: "us3.datadoghq.com"
+			});
 			datadog = new v2.MetricsApi(configuration);
 		}
 
@@ -27,7 +30,7 @@ export class MyDurableObject extends DurableObject {
 			body: {
 				series: [
 					{
-						metric: 'system.cpu.user',
+						metric: 'cloudflare.worker.example',
 						type: 3,
 						points: [
 							{
@@ -39,6 +42,36 @@ export class MyDurableObject extends DurableObject {
 				],
 			},
 		};
+
+
+		// Plain fetch version
+
+		// const payload = {
+		// 	series: [
+		// 	  {
+		// 		metric: "cloudflare.workers.1",
+		// 		type: 0,                                    // 0 = gauge
+		// 		points: [{ timestamp: Math.floor(Date.now() / 1000), value: 0.7 }],
+		// 		resources: [{ name: "dummyhost", type: "host" }],
+		// 	  },
+		// 	],
+		//   };
+
+		//   fetch("https://api.us3.datadoghq.com/api/v2/series", {
+		// 	method: "POST",
+		// 	headers: {
+		// 	  Accept: "application/json",
+		// 	  "Content-Type": "application/json",
+		// 	  "DD-API-KEY": this.env.DATADOG_API_KEY,                // stored as a secret
+		// 	},
+		// 	body: JSON.stringify(payload),
+		//   }).then((res) => res.json()).then((data) => {
+		// 	console.log(data);
+		// }).catch((error) => {
+		// 	console.log("Jfkldsjklfjkldsklj");
+		// 	console.error(error);
+		// })
+
 
 		this.ctx.waitUntil(
 			datadog
